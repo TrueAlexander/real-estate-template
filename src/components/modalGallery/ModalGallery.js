eff1b57c9cf97082ca7a754068ad34c9a4850b00
Alexander <caminante.msk@gmail.com>
2022-08-15 18:05:47 -0300

    flash bug experiment 2.0

--- src/components/modalGallery/ModalGallery.js
+++ src/components/modalGallery/ModalGallery.js
@@ -2,43 +2,51 @@ import './ModalGallery.css'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation} from 'swiper'
 import { useEffect } from 'react'
+import useIsMounted from '../../utils/useIsMounted'
 
 
 
 const ModalGallery = ({active, setActive, photosArr}) => {
+
+  
+  const isMounted = useIsMounted()
+  
+
   
   useEffect(() => {
     active ? document.body.style.overflow = 'hidden' : document.body.style.overflow = ''
-    console.log(active);
+    console.log(active)
   }, [active])
 
   return (
-    <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)} >
-      <div className={active ? "modal-content active" : "modal-content"} onClick={e => e.stopPropagation()} >
-        <h2>Fotos do imóvel</h2>
-        <Swiper
-          className="modal-slider"
-          modules={[Navigation,]}
-          spaceBetween={0}
-          slidesPerView={1}
-          loop={true}
-          navigation
-        >
-          {photosArr.reverse().map((item, index) => {
-            return <SwiperSlide 
-                      key={index} 
-                      className="modal-slide">
-                      <div className="modal-card">
-                        <img 
-                          src={item} 
-                          alt={"property" + index} 
-                        />
-                      </div>
-                    </SwiperSlide>
-            })}
-        </Swiper>
-        <button className='btn' onClick={() => setActive(false)}>Fechar</button>
-      </div> 
+    <div className={`navigation ${isMounted ? 'mounted' : ''}`}>
+      <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)} >
+        <div className={active ? "modal-content active" : "modal-content"} onClick={e => e.stopPropagation()} >
+          <h2>Fotos do imóvel</h2>
+          <Swiper
+            className="modal-slider"
+            modules={[Navigation,]}
+            spaceBetween={0}
+            slidesPerView={1}
+            loop={true}
+            navigation
+          >
+            {photosArr.reverse().map((item, index) => {
+              return <SwiperSlide 
+                        key={index} 
+                        className="modal-slide">
+                        <div className="modal-card">
+                          <img 
+                            src={item} 
+                            alt={"property" + index} 
+                          />
+                        </div>
+                      </SwiperSlide>
+              })}
+          </Swiper>
+          <button className='btn' onClick={() => setActive(false)}>Fechar</button>
+        </div> 
+      </div>
     </div>
   )
 }
